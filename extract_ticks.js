811517033f536aea992a8057a2394c2f3b0e1c00
Thibault <thibault.viglino@epfl.ch>
2018-10-16 14:58:19 +0200

    added files

---
+++ extract_ticks.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var jsgo = require('jsgo');
+
+demo_path = 'data/ast_vp.dem';
+
+
+fs.readFile(demo_path, function(err, data) {
+  var test = new jsgo.Demo().getPlayers();
+  console.log(test);
+  var targ_list = [];
+
+  var jsdem = new jsgo.Demo();
+
+  jsdem.on('game.weapon_fire', function(event) {
+    //console.log(jsdem.getTick());
+ 
+        console.log(event);
+        console.log(this.getTick())
+    
+  });
+
+  jsdem.parse(data);
+
+});
+
